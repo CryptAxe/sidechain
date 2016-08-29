ed1e7c8b447dc3d379250d9fca23e8ffa0f158eb
CryptAxe <cryptaxe@gmail.com>
2016-08-29 11:05:29 -0700

    Update sidechain page, create wt from the GUI, validate

--- src/qt/sidechainpage.cpp
+++ src/qt/sidechainpage.cpp
@@ -1,6 +1,17 @@
 #include "sidechainpage.h"
 #include "ui_sidechainpage.h"
 
+#include "base58.h"
+#include "coins.h"
+#include "guiutil.h"
+#include "init.h"
+#include "main.h"
+#include "primitives/drivechain.h"
+#include "txdb.h"
+#include "wallet/wallet.h"
+
+#include <QApplication>
+#include <QClipboard>
 #include <QStackedWidget>
 
 SidechainPage::SidechainPage(QWidget *parent) :
@@ -60,4 +71,81 @@ void SidechainPage::on_pushButtonCopy_clicked()
 void SidechainPage::on_pushButtonNew_clicked()
 {
 
+}
+
+void SidechainPage::on_pushButtonWT_clicked()
+{
+    if (!validate()) return;
+
+    drivechainWithdraw wt;
+    CBitcoinAddress address(ui->payTo->text().toStdString());
+
+    if (!address.GetKeyID(wt.keyID)) {
+        // TODO ERROR MESSAGE
+        return;
+    }
+
+    wt.sidechainid = SIDECHAIN_ID;
+
+    CReserveKey reserveKey(pwalletMain);
+    CAmount nFeeRequired;
+
+    vector<CRecipient> vecSend;
+    CRecipient recipient = {wt.GetScript(), ui->payAmount->value(), false};
+    vecSend.push_back(recipient);
+
+    int nChangePos = -1;
+
+    CWalletTx wtx;
+    std::string strError;
+    if (!pwalletMain->CreateTransaction(vecSend, wtx, reserveKey, nFeeRequired, nChangePos, strError))
+    {
+        if (nFeeRequired > pwalletMain->GetBalance())
+            return; // TODO ERROR MESSAGE
+    }
+    if (!pwalletMain->CommitTransaction(wtx, reserveKey))
+        return; // TODO ERROR MESSAGE
+}
+
+void SidechainPage::on_addressBookButton_clicked()
+{
+
+}
+
+void SidechainPage::on_pasteButton_clicked()
+{
+    // Paste text from clipboard into recipient field
+    ui->payTo->setText(QApplication::clipboard()->text());
+}
+
+void SidechainPage::on_deleteButton_clicked()
+{
+    ui->payTo->clear();
+}
+
+bool SidechainPage::validate()
+{
+    // Check drivechain index
+    if (!pdrivechaintree) return false;
+
+    // Check address
+    CBitcoinAddress address(ui->payTo->text().toStdString());
+    if (!address.IsValid()) return false;
+
+    if (!ui->payAmount->validate()) return false;
+
+    // Sending a zero amount is invalid
+    if (ui->payAmount->value(0) <= 0)
+    {
+        ui->payAmount->setValid(false);
+        return false;
+    }
+
+    // Reject dust outputs:
+    if (GUIUtil::isDust(ui->payTo->text(), ui->payAmount->value())) {
+        ui->payAmount->setValid(false);
+        return false;
+    }
+
+    return true;
 }
